24c81ff28be9a73456bec1022c4414bffc3934e9
Taufiq Zarif <taufiqzarif90@gmail.com>
2024-03-23 16:35:08 +0800

    implement router, middleware and controller

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
 const port = process.env.PORT || 7000;
 
 connectDB();
@@ -13,13 +15,8 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
