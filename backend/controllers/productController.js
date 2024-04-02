7658f15f9d25f507d29a878dfde04553d9982dcd
Taufiq Zarif <taufiqzarif90@gmail.com>
2024-04-02 19:02:57 +0800

    for production changes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,7 +5,7 @@ import asyncHandler from "../middleware/asyncHandler.js";
 // @route   GET /api/products
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => {
-  const pageSize = 12;
+  const pageSize = process.env.PAGINATION_LIMIT || 12;
   const page = Number(req.query.pageNumber) || 1;
 
   const keyword = req.query.keyword
--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,6 @@ app.use(express.urlencoded({ extended: true }));
 // Middleware: Cookie parser
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -38,6 +34,21 @@ app.get("/api/config/paypal", (req, res) =>
 
 const __dirname = path.resolve(); // Set __dirname to the current working directory
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+
+  // Set frontend build folder as a static folder
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  // Serve the index.html file if it doesn't hit any of the routes above
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
