3eb378e88b4f36fd947f913d1685f6043dbd0940
Taufiq Zarif <taufiqzarif90@gmail.com>
2024-04-11 15:17:07 +0800

    fix

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,12 +1,19 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { BASE_URL } from '../constants';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BASE_URL } from "../constants";
+import { logout } from "./authSlice";
 
 const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
 
+async function baseQueryWithAuth(args, api, extraOptions) {
+  const result = await baseQuery(args, api, extraOptions);
+  if (result.error && result.error.status === 401) {
+    api.dispatch(logout());
+  }
+  return result;
+}
+
 export const apiSlice = createApi({
-    baseQuery,
-    tagTypes: ['Product', 'User', 'Order'],
-    endpoints: (builder) => ({
-        
-    }),
+  baseQuery: baseQueryWithAuth,
+  tagTypes: ["Product", "User", "Order"],
+  endpoints: (builder) => ({}),
 });
